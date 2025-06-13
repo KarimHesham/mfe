0e484f9f98051831ee49a48816797501a53f95df
KarimHesham <karimdeveloper@outlook.com>
2025-06-13 18:00:02 +0300

    refactor: standardize quotes in webpack configuration and component files across container and dashboard packages

--- packages/container/src/components/DashboardApp.js
+++ packages/container/src/components/DashboardApp.js
@@ -1,11 +1,11 @@
 import { mount } from "dashboard/DashboardApp";
 import React, { useRef, useEffect } from "react";
 
-export default ({ onSignIn }) => {
+export default () => {
   const ref = useRef(null);
 
   useEffect(() => {
-    mount(ref.current, { onSignIn });
+    mount(ref.current);
   }, []);
 
   return <div ref={ref} />;
